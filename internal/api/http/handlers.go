eadae63c88b6e6b402db16da527419ee403cb7d0
mohammadne <mohammadne.dev@gmail.com>
2025-01-31 14:10:45 +0330

    refactor: refactor remaining handlers and mv to api/http

--- internal/api/http/handlers.go
+++ internal/api/http/handlers.go
@@ -1,12 +1,16 @@
 package http
 
 import (
+	"errors"
 	"fmt"
+	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"gorm.io/gorm"
 
-	"github.com/mohammadne/ice-global/internal/calculator"
 	"github.com/mohammadne/ice-global/internal/db"
 	"github.com/mohammadne/ice-global/internal/entity"
 )
@@ -26,9 +30,10 @@ func (s *Server) showAddItemForm(c *gin.Context) {
 		"Error": c.Query("error"),
 	}
 
-	cookie, exists := c.Get(CookieName)
+	cookieRaw, exists := c.Get(CookieName)
 	if exists {
-		data["CartItems"] = getCartItemData(cookie.(*http.Cookie).Value)
+		cookie := cookieRaw.(*http.Cookie)
+		data["CartItems"] = getCartItemData(cookie.Value)
 	}
 
 	c.HTML(http.StatusOK, "index.html", data)
@@ -64,12 +69,157 @@ func getCartItemData(sessionID string) (items []map[string]interface{}) {
 
 // -----------------------------------------------------
 
+var itemPriceMapping = map[string]float64{
+	"shoe":  100,
+	"purse": 200,
+	"bag":   300,
+	"watch": 300,
+}
+
+type CartItemForm struct {
+	Product  string `form:"product"   binding:"required"`
+	Quantity string `form:"quantity"  binding:"required"`
+}
+
 func (s *Server) addItem(c *gin.Context) {
-	calculator.AddItemToCart(c)
+	cookieRaw, exists := c.Get(CookieName)
+	if !exists {
+		c.JSON(http.StatusUnauthorized, nil) // TODO
+		return
+	}
+	cookie := cookieRaw.(*http.Cookie)
+
+	db := db.GetDatabase()
+
+	var isCartNew bool
+	var cartEntity entity.CartEntity
+	result := db.Where(fmt.Sprintf("status = '%s' AND session_id = '%s'", entity.CartOpen, cookie.Value)).First(&cartEntity)
+
+	if result.Error != nil {
+		if !errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			c.Redirect(302, "/")
+			return
+		}
+		isCartNew = true
+		cartEntity = entity.CartEntity{
+			SessionID: cookie.Value,
+			Status:    entity.CartOpen,
+		}
+		db.Create(&cartEntity)
+	}
+
+	addItemForm, err := getCartItemForm(c)
+	if err != nil {
+		c.Redirect(302, "/?error="+err.Error())
+		return
+	}
+
+	item, ok := itemPriceMapping[addItemForm.Product]
+	if !ok {
+		c.Redirect(302, "/?error=invalid item name")
+		return
+	}
+
+	quantity, err := strconv.ParseInt(addItemForm.Quantity, 10, 0)
+	if err != nil {
+		c.Redirect(302, "/?error=invalid quantity")
+		return
+	}
+
+	var cartItemEntity entity.CartItem
+	if isCartNew {
+		cartItemEntity = entity.CartItem{
+			CartID:      cartEntity.ID,
+			ProductName: addItemForm.Product,
+			Quantity:    int(quantity),
+			Price:       item * float64(quantity),
+		}
+		db.Create(&cartItemEntity)
+	} else {
+		result = db.Where(" cart_id = ? and product_name  = ?", cartEntity.ID, addItemForm.Product).First(&cartItemEntity)
+
+		if result.Error != nil {
+			if !errors.Is(result.Error, gorm.ErrRecordNotFound) {
+				c.Redirect(302, "/")
+				return
+			}
+			cartItemEntity = entity.CartItem{
+				CartID:      cartEntity.ID,
+				ProductName: addItemForm.Product,
+				Quantity:    int(quantity),
+				Price:       item * float64(quantity),
+			}
+			db.Create(&cartItemEntity)
+
+		} else {
+			cartItemEntity.Quantity += int(quantity)
+			cartItemEntity.Price += item * float64(quantity)
+			db.Save(&cartItemEntity)
+		}
+	}
+
+	c.Redirect(302, "/")
+}
+
+func getCartItemForm(c *gin.Context) (*CartItemForm, error) {
+	if c.Request.Body == nil {
+		return nil, fmt.Errorf("body cannot be nil")
+	}
+
+	form := &CartItemForm{}
+
+	if err := binding.FormPost.Bind(c.Request, form); err != nil {
+		log.Println(err.Error())
+		return nil, err
+	}
+
+	return form, nil
 }
 
 // -----------------------------------------------------
 
 func (s *Server) deleteCartItem(c *gin.Context) {
-	calculator.DeleteCartItem(c)
+	cookieRaw, exists := c.Get(CookieName)
+	if !exists {
+		c.JSON(http.StatusUnauthorized, nil) // TODO
+		return
+	}
+	cookie := cookieRaw.(*http.Cookie)
+
+	cartItemIDString := c.Query("cart_item_id")
+	if cartItemIDString == "" {
+		c.Redirect(302, "/")
+		return
+	}
+
+	db := db.GetDatabase()
+
+	var cartEntity entity.CartEntity
+	result := db.Where(fmt.Sprintf("status = '%s' AND session_id = '%s'", entity.CartOpen, cookie.Value)).First(&cartEntity)
+	if result.Error != nil {
+		c.Redirect(302, "/")
+		return
+	}
+
+	if cartEntity.Status == entity.CartClosed {
+		c.Redirect(302, "/")
+		return
+	}
+
+	cartItemID, err := strconv.Atoi(cartItemIDString)
+	if err != nil {
+		c.Redirect(302, "/")
+		return
+	}
+
+	var cartItemEntity entity.CartItem
+
+	result = db.Where(" ID  = ?", cartItemID).First(&cartItemEntity)
+	if result.Error != nil {
+		c.Redirect(302, "/")
+		return
+	}
+
+	db.Delete(&cartItemEntity)
+	c.Redirect(302, "/")
 }
